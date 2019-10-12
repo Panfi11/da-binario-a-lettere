d2422439cf996298ac5a3c9157167d1a2e854bff
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-12 11:41:57 +0200

    Add project files.

---
+++ Binario lettere/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Binario_lettere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un nome");
+            string s;
+            Console.WriteLine(" Enter your sentence: ");
+            s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                Console.WriteLine((int)c);
+            }
+            Console.ReadLine();
+
+        }
+    }
+ }
